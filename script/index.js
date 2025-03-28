dc49361598173ba2e78daca2234b7b983c9b133e
iamyoures <iamyoures@gmail.com>
2025-03-28 14:27:45 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,10 +1,21 @@
-
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
+
+
+
+
+const menuLinks = document.querySelectorAll('.mobile-menu a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('open');
+    });
+});
+
 
 
 
